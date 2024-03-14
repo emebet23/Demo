7809af6f9ae9e17b960920daa13c195cf97dfe86
emebet23 <emebetsisay120@gmail.com>
2024-03-14 06:00:41 -0700

    add javascript

---
+++ app.js
@@ -0,0 +1,6 @@
+const changeer = document.getElementById("changeer");
+const button = document.getElementsByTagName(" button");
+button.addEventListener("click, displayDate");
+function displayDate() {
+  changeer.style.background;
+}
